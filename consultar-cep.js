00f0bfd1697e79270828f592076dfa9dc0859b8e
ErickJulio <erickwiejulio@gmail.com>
2023-10-12 20:24:32 -0300

    teste

---
+++ consultar-cep.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const consultarCepButton = document.getElementById('consultarCepButton');
+    const cepInput = document.getElementById('cepInput');
+    const resultadoCep = document.getElementById('resultadoCep');
+
+    consultarCepButton.addEventListener('click', function() {
+        const cep = cepInput.value;
+        if (cep) {
+            consultarCep(cep);
+        } else {
+            resultadoCep.textContent = 'Digite um CEP válido.';
+        }
+    });
+
+    function consultarCep(cep) {
+        fetch('http://localhost:3000/validar-cep', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                cep: cep
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.mensagem) {
+                resultadoCep.textContent = data.mensagem;
+            } else {
+                resultadoCep.innerHTML = `<strong>CEP:</strong> ${cep}<br>`;
+                resultadoCep.innerHTML += `<strong>Estado:</strong> ${data.state}<br>`;
+                resultadoCep.innerHTML += `<strong>Cidade:</strong> ${data.city}<br>`;
+                resultadoCep.innerHTML += `<strong>Bairro:</strong> ${data.neighborhood}<br>`;
+                resultadoCep.innerHTML += `<strong>Rua:</strong> ${data.street}<br>`;
+                resultadoCep.innerHTML += `<strong>Serviço:</strong> ${data.service}`;
+            }
+        })
+        .catch(error => {
+            resultadoCep.textContent = 'Erro na consulta do CEP.';
+        });
+    }
+});
---
+++ main.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const gerarDadosButton = document.getElementById('gerarDadosButton');
+    const apagarHistoricoButton = document.getElementById('apagarHistoricoButton');
+    const tabelaCorpo = document.getElementById('tabelaCorpo');
+
+    gerarDadosButton.addEventListener('click', function() {
+        fazGet('http://localhost:3000/gerar-dadosAleatorios', function(usuario) {
+            atualizarTabela(usuario);
+        });
+    });
+
+    apagarHistoricoButton.addEventListener('click', function() {
+        apagarHistorico();
+    });
+
+    function fazGet(url, callback) {
+        let request = new XMLHttpRequest();
+        request.open('GET', url, true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                const data = JSON.parse(request.responseText);
+                callback(data);
+            } else {
+                console.error('Erro ao fazer a requisição: ' + request.status);
+            }
+        };
+
+        request.onerror = function() {
+            console.error('Erro de conexão');
+        };
+
+        request.send();
+    }
+
+    function atualizarTabela(usuario) {
+        const novaLinha = document.createElement('tr');
+
+        const nomeCell = document.createElement('td');
+        nomeCell.textContent = usuario.nome;
+
+        const cpfCell = document.createElement('td');
+        cpfCell.textContent = usuario.cpf;
+
+        const dataNascimentoCell = document.createElement('td');
+        dataNascimentoCell.textContent = usuario.dataNascimento;
+
+        novaLinha.appendChild(nomeCell);
+        novaLinha.appendChild(cpfCell);
+        novaLinha.appendChild(dataNascimentoCell);
+
+        tabelaCorpo.appendChild(novaLinha);
+    }
+
+    function apagarHistorico() {
+        while (tabelaCorpo.firstChild) {
+            tabelaCorpo.removeChild(tabelaCorpo.firstChild);
+        }
+    }
+
+    // Inicialize a tabela preenchendo dados quando a página carregar
+    fazGet('http://localhost:3000/gerar-dadosAleatorios', function(usuario) {
+        atualizarTabela(usuario);
+    });
+});
