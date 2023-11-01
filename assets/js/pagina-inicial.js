273df807e159d31dc405ad00f82daea4706751d2
ErickJulio <erickwiejulio@gmail.com>
2023-11-01 20:10:35 -0300

    teste

--- assets/js/pagina-inicial.js
+++ assets/js/pagina-inicial.js
@@ -1,4 +1,34 @@
-const botao = document.querySelector(".btn");
-botao.addEventListener("click", function () {
-    window.location.href = "pagina-principal.html";
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("login-form");
+    const messageElement = document.getElementById("message");
+
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const usuario = document.getElementById("usuario").value;
+        const senha = document.getElementById("senha").value;
+
+        fetch("https://api-teste-dados.onrender.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ usuario, senha }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.mensagem === "Login bem-sucedido") {
+                messageElement.innerHTML = "Login bem-sucedido!";
+                messageElement.style.color = "green";
+                // Redirecionar para a página principal após o login bem-sucedido (substitua 'pagina-principal.html' pelo URL correto)
+                window.location.href = 'pagina-principal.html';
+            } else {
+                messageElement.innerHTML = "Credenciais inválidas";
+                messageElement.style.color = "red";
+            }
+        })
+        .catch((error) => {
+            console.error("Erro:", error);
+        });
+    });
 });
