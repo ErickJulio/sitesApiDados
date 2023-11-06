fccf8183a3a965ad32aa94c6e81dc92a61a741d5
ErickJulio <erickwiejulio@gmail.com>
2023-11-05 22:28:24 -0300

    ajusteSenha

--- assets/js/pagina-inicial.js
+++ assets/js/pagina-inicial.js
@@ -11,20 +11,32 @@ document.addEventListener("DOMContentLoaded", function () {
         fetch("https://api-teste-dados.onrender.com/login", {
             method: "POST",
             headers: {
+                "accept": "application/json",
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ usuario, senha }),
+            body: JSON.stringify({ login: usuario, senha }),
         })
-        .then((response) => response.json())
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else if (response.status === 401) {
+                // Senha incorreta
+                messageElement.innerHTML = "Senha incorreta. Tente novamente.";
+                messageElement.style.color = "red";
+            } else if (response.status === 500) {
+                // Erro no servidor
+                messageElement.innerHTML = "Erro no servidor. Por favor, tente novamente mais tarde.";
+                messageElement.style.color = "red";
+            } else {
+                throw new Error("Erro na solicitação.");
+            }
+        })
         .then((data) => {
-            if (data.mensagem === "Login bem-sucedido") {
-                messageElement.innerHTML = "Login bem-sucedido!";
+            if (data.message === "Login bem-sucedido!") {
+                messageElement.innerHTML = "Login bem-sucedido! Redirecionando para a página principal...";
                 messageElement.style.color = "green";
                 // Redirecionar para a página principal após o login bem-sucedido (substitua 'pagina-principal.html' pelo URL correto)
                 window.location.href = 'pagina-principal.html';
-            } else {
-                messageElement.innerHTML = "Credenciais inválidas";
-                messageElement.style.color = "red";
             }
         })
         .catch((error) => {
